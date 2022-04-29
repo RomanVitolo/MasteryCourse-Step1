5f1ccee633e7cdaf29eab6b6e3d6ea6dd4bd84be
Roman Vitolo <68485569+RomanVitolo@users.noreply.github.com>
2022-04-29 01:36:39 -0300

    PlayerMovement2D + Prefab

--- Assets/Scripts/AgentController.cs
+++ Assets/Scripts/AgentController.cs
@@ -1,24 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AgentGrounding))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class AgentController : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
-    
-    void Start()
+    [SerializeField] private float jumpForce = 200f;
+    Rigidbody2D _rb;
+    AgentGrounding _agentGrounding;
+    SpriteRenderer _spriteRenderer;
+
+    private void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
+        _agentGrounding = GetComponent<AgentGrounding>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        SetInput();
+        if (Input.GetButtonDown("Fire1") && _agentGrounding.IsGrounded)
+        {
+            _rb.AddForce(Vector2.up * jumpForce);
+        }
+        Debug.Log(_agentGrounding.IsGrounded);
+    }
+    
+    void SetInput()
+    { 
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(horizontal, vertical);
+        Vector3 movement = new Vector3(horizontal, 0);
         //transform.Translate(movement * speed * Time.deltaTime);
         transform.position += movement * speed * Time.deltaTime;
+        
+        
     }
 }
---
+++ Assets/Scripts/AgentGrounding.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentGrounding : MonoBehaviour
+{
+    [SerializeField] private Transform _leftFoot;
+    [SerializeField] private Transform _rightFoot;
+    [SerializeField] private LayerMask _layerMask;
+    [SerializeField] float maxDistance = 3f;
+    public bool IsGrounded{ get; private set; }
+   
+    void Update()
+    {
+        CheckFootForGrounding(_leftFoot);
+        if (IsGrounded == false)
+        {
+            CheckFootForGrounding(_rightFoot);
+        }
+    }
+
+    private void CheckFootForGrounding(Transform _foot)
+    {
+        var rayCastHit = Physics2D.Raycast(_foot.position, Vector2.down, maxDistance, _layerMask);
+        Debug.DrawRay(_foot.position, Vector3.down * maxDistance, Color.red);
+
+        if (rayCastHit.collider != null)
+        {
+            IsGrounded = true;
+        }
+        else
+        {
+            IsGrounded = false;
+        }
+    }
+}
