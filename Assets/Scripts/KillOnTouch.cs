48a2f193a13907aff1c47098d24342ef35e810fa
Roman Vitolo <68485569+RomanVitolo@users.noreply.github.com>
2022-04-30 03:33:32 -0300

    Import 2D Platform Assets

---
+++ Assets/Scripts/KillOnTouch.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillOnTouch : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        var agenControler = other.collider.GetComponent<AgentController>();
+        if (agenControler != null)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
